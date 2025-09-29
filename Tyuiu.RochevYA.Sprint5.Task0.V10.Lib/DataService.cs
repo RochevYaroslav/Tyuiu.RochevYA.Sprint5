bdfe0e3ae22f4178957bb81e7d3186248d87585f
RochevYaroslav <justmisterstone@gmail.com>
2025-09-30 00:14:49 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.RochevYA.Sprint5.Task0.V10.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RochevYA.Sprint5.Task0.V10.Lib
+{
+    public class DataService : ISprint5Task0V10
+    {
+        public string SaveToFileTextData(int x)
+        {
+            double y = 1.6*Math.Pow(x,3) - 2.1*Math.Pow(x,2) + 7*x;
+            string outputFile = "OutPutFileTask0.txt";
+            File.WriteAllText(outputFile, y.ToString());
+            return y.ToString();
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint5.Task0.V10/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.RochevYA.Sprint5.Task0.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 2;
+        Console.WriteLine(ds.SaveToFileTextData(x));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
