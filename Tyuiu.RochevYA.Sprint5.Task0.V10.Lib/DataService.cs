58431e55e3b918dec10864fd45f9aa9b0204378d
RochevYaroslav <justmisterstone@gmail.com>
2025-09-30 00:17:24 +0500

    Task0

--- Tyuiu.RochevYA.Sprint5.Task0.V10.Lib/DataService.cs
+++ Tyuiu.RochevYA.Sprint5.Task0.V10.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.RochevYA.Sprint5.Task0.V10.Lib
         {
             double y = 1.6*Math.Pow(x,3) - 2.1*Math.Pow(x,2) + 7*x;
             string outputFile = "OutPutFileTask0.txt";
-            File.WriteAllText(outputFile, y.ToString());
+            //File.WriteAllText(outputFile, y.ToString());
             return y.ToString();
         }
     }
