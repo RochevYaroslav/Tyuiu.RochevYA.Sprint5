be39987a35c25c8244da2b7d7305085cc14067d1
RochevYaroslav <justmisterstone@gmail.com>
2025-09-30 22:29:59 +0500

    Task1

---
+++ Tyuiu.RochevYA.Sprint5.Task1.V5.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RochevYA.Sprint5.Task1.V5.Lib
+{
+    public class DataService : ISprint5Task1V5
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            List<int> results = new List<int>();
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double denominator = 2 * i - 0.5;
+
+                if (Math.Abs(denominator) < 1e-10)
+                {
+                    results.Add(0);
+                }
+                else
+                {
+                    double value = 5 - 3 * i + (1 + Math.Sin(i)) / denominator;
+                    results.Add((int)Math.Round(value)); // или сохранять как double?
+                }
+            }
+
+            string filePath = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+            //File.WriteAllLines(filePath, results.Select(x => x.ToString()));
+            return string.Join("\n", results);
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint5.Task1.V5/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.RochevYA.Sprint5.Task1.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int start = -5;
+        int end = 5;
+        Console.WriteLine(ds.SaveToFileTextData(start,end));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
