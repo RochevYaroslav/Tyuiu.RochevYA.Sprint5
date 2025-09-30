45933f43e2b863b7574ff10ce28bb0915e30bee0
RochevYaroslav <justmisterstone@gmail.com>
2025-09-30 22:47:36 +0500

    Task2

---
+++ Tyuiu.RochevYA.Sprint5.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RochevYA.Sprint5.Task2.V5.Lib
+{
+    public class DataService : ISprint5Task2V5
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            var array = matrix;
+            string[] result = new string[3];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string[] row = new string[3];
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (array[i, j] % 2 == 1)
+                    {
+                        array[i, j] = 0;
+                    }
+                    row[j] = array[i, j].ToString();
+                }
+                result[i] = string.Join("; ", row);
+            }
+            return string.Join("\n", result);
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint5.Task2.V5/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.RochevYA.Sprint5.Task2.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int[,] matrix = new int[3,3];
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine(ds.SaveToFileTextData(matrix));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
