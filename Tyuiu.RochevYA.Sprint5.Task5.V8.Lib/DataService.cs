73b4930529c72f5f757edabdb2948b392965c095
RochevYaroslav <justmisterstone@gmail.com>
2025-10-01 07:50:54 +0500

    Task5

---
+++ Tyuiu.RochevYA.Sprint5.Task5.V8.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.RochevYA.Sprint5.Task5.V8.Lib
+{
+    public class DataService : ISprint5Task5V8
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string x = File.ReadAllText(path);
+            x = x.Replace('.', ',');
+            double[] doubleArray = x.Split(" ").Select(x => double.Parse(x)).ToArray();
+
+            double min = 10000000000000;
+            for (int i = 0;i< doubleArray.Length; i++)
+            {
+                if (doubleArray[i] < min)
+                {
+                    min = doubleArray[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
